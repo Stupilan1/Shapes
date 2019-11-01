43c6fb5fd96000cc106a6ae50426e7ce5bf1d4bf
Stupilan1 <stupilan@icloud.com>
2019-11-01 14:29:59 +0000

    Split the form class to make a shapes class. This is to be able to inherit values from the shapes class, instead of all the data in the forms class.

--- GraphicalProgramingLanguage/Form1.cs
+++ GraphicalProgramingLanguage/Form1.cs
@@ -13,15 +13,8 @@ namespace GraphicalProgramingLanguage
 {
     public partial class Form1 : Form
     {
-        enum Shapes
-        {
-            Ellipse,
-            Rectangle,
-            Line
-        }
 
-        int x, y, w, h;
-        Shapes shape;
+        Shape shape;
 
 
         public Form1()
@@ -32,54 +25,45 @@ namespace GraphicalProgramingLanguage
 
         private void button1_Click(object sender, EventArgs e)
         {
-            
-           
-            var cmd = txtB.Text.Split(',');
-            var validCmds = Enum.GetNames(typeof(Shapes));
-
-            if (cmd.Length < 5 || !validCmds.Contains(cmd[0], StringComparer.OrdinalIgnoreCase))
+            shape = null;
+            if (!Shape.Helper(txtB.Text, out shape))
             {
-                MessageBox.Show("Enter a valid command.");
                 return;
             }
-
-            if (!int.TryParse(cmd[1], out x) ||
-                !int.TryParse(cmd[2], out y) ||
-                !int.TryParse(cmd[3], out w) ||
-                !int.TryParse(cmd[4], out h))
-            {
-                MessageBox.Show("Enter a valid shape");
-                return;
-            }
-            shape = (Shapes)validCmds.ToList().FindIndex(a => a.Equals(cmd[0], StringComparison.OrdinalIgnoreCase));
             pictureBox1.Invalidate();
-
         }
 
-       
         private void pictureBox1_Paint(object sender, PaintEventArgs e)
         {
-            if (w > 0 && h > 0)
+            e.Graphics.Clear(Color.White);
+            e.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            if (shape != null)
             {
-                e.Graphics.Clear(Color.White);
-                e.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
 
-                using (Pen pn = new Pen(Color.Black, 5))
+
+                using (Pen MyPen = new Pen(Color.Black, 3))
                 {
-                    switch (shape)
+                    switch (shape.CurrentShape)
                     {
-                        case Shapes.Ellipse:
-                            e.Graphics.DrawEllipse(pn, x, y, w, h);
+                        case Shape.shapes.Ellipse:
+                            e.Graphics.DrawEllipse(MyPen, shape.X, shape.Y, shape.Width, shape.Height);
                             break;
-                        case Shapes.Rectangle:
-                            e.Graphics.DrawRectangle(pn, x, y, w, h);
+                        case Shape.shapes.Rectangle:
+                            e.Graphics.DrawRectangle(MyPen, shape.X, shape.Y, shape.Width, shape.Height);
                             break;
-                        case Shapes.Line:
-                            e.Graphics.DrawLine(pn, x, y, w, h);
+                        case Shape.shapes.Line:
+                            e.Graphics.DrawLine(MyPen, shape.X, shape.Y, shape.Width, shape.Height);
                             break;
                     }
                 }
             }
         }
+
+
+
+
+
+
+
     }
-}
+ }
--- GraphicalProgramingLanguage/Shape.cs
+++ GraphicalProgramingLanguage/Shape.cs
@@ -1,32 +1,87 @@
-﻿/*using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace GraphicalProgramingLanguage
 {
-    abstract class Shape
+    class Shape
     {
 
-        protected Color colour; //shape's colour
-        protected int x, y; //not I could use c# properties for this
-        public Shape(Color colour, int x, int y)
+        public enum shapes
         {
-
-            this.colour = colour; //shape's colour
-            this.x = x; //its x pos
-            this.y = y; //its y pos
-            //can't provide anything else as "shape" is too general
+            Rectangle,
+            Line,
+            Ellipse
         }
 
-        public abstract void draw(Graphics g); //any derrived class must implement this method
-
-        public override string ToString()
+        public Shape(shapes shape, int x, int y, int width, int height)
         {
-            return base.ToString()+"    "+this.x+","+this.y+" : ";
+            CurrentShape = shape;
+            X = x;
+            Y = y;
+            Width = width;
+            Height = height;
         }
+
+        public shapes CurrentShape { get; set; } = shapes.Rectangle;
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public static bool Helper(string input, out Shape output)
+        {
+            output = null;
+
+            if (string.IsNullOrEmpty(input))
+                return false;
+
+            var Commmad = input.Split(' ');
+            var validCommmads = Enum.GetNames(typeof(shapes));
+
+            if (Commmad.Length < 5 || !validCommmads.Contains(Commmad[0], StringComparer.OrdinalIgnoreCase))
+            {
+                MessageBox.Show("Enter Valid Shape");
+                return false;
+            }
+            int x, y, w, h;
+
+            if (!int.TryParse(Commmad[1], out x) ||
+                !int.TryParse(Commmad[2], out y) ||
+                !int.TryParse(Commmad[3], out w) ||
+                !int.TryParse(Commmad[4], out h))
+            {
+                MessageBox.Show("Enter Valid Location/Measurements");
+                return false;
+            }
+            if (w <= 0 || h <= 0)
+            {
+                MessageBox.Show("Height/Width must be greater than Zero");
+                return false;
+            }
+            var shape = (shapes)validCommmads.ToList().FindIndex(a => a.Equals(Commmad[0], StringComparison.OrdinalIgnoreCase));
+            output = new Shape(shape, x, y, w, h);
+            return true;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     }
-}*/
+}
